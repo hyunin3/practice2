1f06b130fbcc6ffabe3fcc51e2c5ed0f76885703
hyunin3 <stock4share2@gmail.com>
2023-02-07 17:53:59 +0900

    0207

--- Bubble_sort.py
+++ Bubble_sort.py
@@ -2,8 +2,8 @@ T = int(input())
 for tc in range(1, T + 1):
     N = int(input())
     arr = list(map(int, input().split()))
-    for i in range(N -1, 0, -1):
-        for j in range(i):
+    for i in range(N):
+        for j in range(N-1):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
     
