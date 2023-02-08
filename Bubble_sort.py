1f9b5bcfd00e5ab651d64cb18c20ef438d707a85
hyunin3 <stock4share2@gmail.com>
2023-02-08 17:22:19 +0900

    0208

--- Bubble_sort.py
+++ Bubble_sort.py
@@ -9,3 +9,16 @@ for tc in range(1, T + 1):
     
 
     print(f'#{tc}',*arr)        
+    
+    
+################
+
+lst=[2,3,5,1,2,7,4,8,9,5,0,1]
+N = len(lst)
+def bubblesort(lst, N):
+    for i in range(N):
+        for j in range(N-1):
+            if lst[j]>lst[j+1]:
+                lst[j], lst[j + 1] = lst[j + 1], lst[j]
+    return lst
+print(bubblesort(lst, N))    
--- 문자열의 거울상.py
+++ 문자열의 거울상.py
@@ -5,3 +5,16 @@ for tc in range(1, T + 1):
     # 문자열 역순으로 하고 p는 q로 b는 d로
     mirror = str.maketrans('pqbd', 'qpdb')
     print(f'#{tc} {N.translate(mirror)}')
+    
+    # N = list(input())
+    # N = N[::-1]
+    # for i in range(len(N)):
+    #     if N[i] == 'p':
+    #         N[i] = 'q'
+    #     elif N[i] == 'q':
+    #         N[i] = 'p'
+    #     elif N[i] == 'b':
+    #         N[i] = 'd'
+    #     elif N[i] == 'd':
+    #         N[i] = 'b'
+    # N = ''.join(N)
