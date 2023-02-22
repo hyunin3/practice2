9099f88e5d4f701d5e583268351ad331c1f08fa2
hyunin3 <stock4share2@gmail.com>
2023-02-22 09:27:26 +0900

    0222

---
+++ 농작물 수확.py
@@ -0,0 +1,17 @@
+T = int(input())
+for tc in range(1, T + 1):
+    N = int(input())
+    arr = [list(map(int, input())) for _ in range(N)]
+    ans = 0
+    s, e = N//2, N//2
+    for i in range(N):
+        for j in range(s, e + 1):
+            ans += arr[i][j]
+        if i < N//2:
+            s -= 1
+            e += 1
+        else:
+            s += 1
+            e -= 1
+
+    print(f'#{tc} {ans}')
