610911685e83fa8c7ed0868ed53d1743938993bf
hyunin3 <stock4share2@gmail.com>
2023-02-23 17:56:59 +0900

    aa

--- 영식이와 친구들.py
+++ 영식이와 친구들.py
@@ -1,21 +1,25 @@
+import sys
+sys.stdin = open("input.txt", "r")
 N, M, L = map(int, input().split())
 cnt = [0]*(N+1)
 s=1
 cnt[1] = 1
+if M == 1:
+    print(0)
+else:
+    while True:
+        s = s + L
+        if s > N:
+            s = s - N
+            cnt[s] += 1
 
-while True:
-    s = s + L
-    if s > N:
-        s = s - N
-        cnt[s] += 1
+            if cnt[s] == M:
+                break
 
-        if cnt[s] == M:
-            break
+        else:
+            cnt[s] += 1
+            ans = 0
+            if cnt[s] == M:
+                break
 
-    else:
-        cnt[s] += 1
-        ans = 0
-        if cnt[s] == M:
-            break
-
-print(sum(cnt) - 1)
+    print(sum(cnt) - 1)
