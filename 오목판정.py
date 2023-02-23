2af8879fade08723bdfa15837cf6a7e80203874e
hyunin3 <stock4share2@gmail.com>
2023-02-23 11:05:54 +0900

    0223

--- 오목판정.py
+++ 오목판정.py
@@ -1,11 +1,10 @@
-di = [-1, 0, 1, 1]
-dj = [1, 1, 1, 0]
 def solve(arr):   
     for si in range(1, N+1):
         for sj in range(1, N+1):
             # 4방향
+            for di, dj in ((1, 0), (-1, 1), (0, 1), (1, 1)):               
                 for mul in range(5):
-                    ni,nj = si+di*mul, sj+dj*mul
+                    ni, nj = si+di*mul, sj+dj*mul
                     if arr[ni][nj] !='o':
                         break
                 else:       # 모두 'o'
