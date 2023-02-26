5363eaa5069b02e2c51818ce63a0ba8a80cb1b7c
hyunin3 <stock4share2@gmail.com>
2023-02-26 18:54:50 +0900

    0226

---
+++ 숫자 배열 회전.py
@@ -0,0 +1,15 @@
+for test_case in range(1, T + 1):
+    N = int(input())
+    arr = [input().split() for _ in range(N)]
+    a1 = [[0]*N for _ in range(N)]  
+    a2 = [[0]*N for _ in range(N)]
+    a3 = [[0]*N for _ in range(N)]
+
+    for i in range(N):
+        for j in range(N):
+            a1[i][j] = arr[N-1-j][i]
+            a2[i][j] = arr[N-1-i][N-1-j]
+            a3[i][j] = arr[j][N-1-i]
+    print(f'#{test_case}')
+    for a,b,c in zip(a1,a2,a3):
+        print(f'{"".join(a)} {"".join(b)} {"".join(c)}')
