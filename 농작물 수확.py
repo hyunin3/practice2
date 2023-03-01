f31da57e9a0064c7ce43ad86986575e798578cc8
hyunin3 <stock4share2@gmail.com>
2023-03-01 12:45:36 +0900

    0301

--- 농작물 수확.py
+++ 농작물 수확.py
@@ -2,16 +2,16 @@ T = int(input())
 for tc in range(1, T + 1):
     N = int(input())
     arr = [list(map(int, input())) for _ in range(N)]
-    ans = 0
-    s, e = N//2, N//2
+    mid = N//2
+    s = e = mid
+    sm = 0
     for i in range(N):
         for j in range(s, e + 1):
-            ans += arr[i][j]
-        if i < N//2:
+            sm += arr[i][j]
+        if i < mid:
             s -= 1
             e += 1
         else:
-            s += 1
+            s += 1        
             e -= 1
-
-    print(f'#{tc} {ans}')
+    print(f'#{tc} {sm}')        
--- 오목판정.py
+++ 오목판정.py
@@ -17,3 +17,27 @@ for tc in range(1, T + 1):
     arr = ['.'*(N+2)]+['.'+input()+'.' for _ in range(N)]+['.'*(N+2)]
     ans = solve(arr)
     print(f'#{tc} {ans}')
+
+#################
+def is_valid(i, j):
+    return 0<=i<N and 0<=j<N
+def solve(arr):
+    for si in range(N):
+        for sj in range(N):
+            if arr[si][sj] == 'o':
+                for di, dj in ((-1, 1), (0, 1), (1, 1), (1, 0)):
+                    for mul in range(5):
+                        ni, nj = si + di*mul, sj + dj * mul
+
+                        if not is_valid(ni, nj) or arr[ni][nj] == '.':
+                            break
+                    else:
+                        return 'YES'
+    return 'NO'                    
+                    
+T = int(input())
+for tc in range(1, T + 1):
+    N = int(input())
+    arr = [list(input()) for _ in range(N)]
+    
+    print(f'#{tc} {solve(arr)}')    
