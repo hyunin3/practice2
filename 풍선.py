47c4856db296bfcf610f53d3859afc1de696e62e
hyunin3 <stock4share2@gmail.com>
2023-03-02 17:10:13 +0900

    0302

---
+++ 풍선.py
@@ -0,0 +1,18 @@
+def is_valid(i, j):
+    return 0<=i<N and 0<=j<N
+
+T = int(input())
+for tc in range(1, T + 1):
+    N, M = map(int, input().split())
+    arr = [list(map(int, input().split())) for _ in range(N)]
+    ans = 0
+    for si in range(N):
+        for sj in range(M):
+            sm = arr[si][sj]
+            for di, dj in ((1, 0), (-1, 0), (0, -1), (0, 1)):
+                ni, nj = si + di, sj + dj
+                if is_valid(ni, nj):
+                    sm += arr[ni][nj]
+            if ans < sm:
+                ans = sm
+    print(f'#{tc} {ans}')                    
