2e729b5dc7c6177538bd21bf5e6bc8a83ff9f944
hyunin3 <stock4share2@gmail.com>
2023-03-03 14:34:40 +0900

    0303

--- magnetic.py
+++ magnetic.py
@@ -1,16 +1,15 @@
-T = 1
+T = 10
 for tc in range(1, T + 1):
     N = int(input())
     arr = [list(map(int, input().split())) for _ in range(N)]
+    arr_t = list(map(list, zip(*arr)))
     ans = 0
-    for j in range(N):
-        cnt = 0
-        for i in range(N):
-            if arr[i][j] == 1:
-                cnt = 1
-            elif arr[i][j] == 2:
-                if cnt:
+    for lst in arr_t:
+        prev = 0
+        for i in lst:
+            if i:
+                if i == 2 and prev == 1:
                     ans += 1
-                    cnt = 0                          
-                  
-    print(f'#{tc} {ans}')                         
+                prev = i
+ 
+    print(f'#{tc} {ans}')
--- view.py
+++ view.py
@@ -11,3 +11,14 @@ for tc in range(1, T + 1):
 
 
     print(f'#{tc} {cnt}')
+###########################
+T = 10
+for tc in range(1, T + 1):
+    N = int(input())
+    lst = list(map(int, input().split()))
+    cnt = 0
+    for i in range(2, N-2):
+        if lst[i] > max(lst[i-2], lst[i-1], lst[i+1], lst[i+2]):
+            cnt += lst[i] - max(lst[i-2], lst[i-1], lst[i+1], lst[i+2])
+    print(f'#{tc} {cnt}')
+    #이렇게 해도 되는데 시간은 더 오래 걸림    
